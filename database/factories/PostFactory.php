d09af084835f7cc307676d70d1b0a3cec6884d06
nixtone <nixtone@nxt.ru>
2023-03-29 15:10:40 +0300

    Модель, миграция, фабрика, сид

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $name = $this->faker->title();
+        return [
+            'name' => $name,
+            'translit' => Str::slug($name),
+            'anons' => $this->faker->realText(150),
+            'text' => $this->faker->realText(800),
+
+        ];
+    }
+}
